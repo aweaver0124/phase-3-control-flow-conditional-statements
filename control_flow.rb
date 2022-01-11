afcc55fd4acafb85c16b1f98ea2fe42b3bfae394
Alexandra Weaver <aweaver0124@gmail.com>
2022-01-10 23:19:17 -0600

    commit tree d73cfd88ca9fe261f7918fa59ee551c9d9c815fb

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,19 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin" || "ADMIN" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
 end
 
-def hows_the_weather(temperature)
-  # your code here
-end
+# def hows_the_weather(temperature)
+#   # your code here
+# end
 
-def fizzbuzz(num)
-  # your code here
-end
+# def fizzbuzz(num)
+#   # your code here
+# end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
+# def calculator(operation, num1, num2)
+#   # your code here
+# end
 
